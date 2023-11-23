943fddb655bbc64ea61aaf5e65ac72301bf89d30
David Livingston <git@dliv.org>
2023-11-23 16:32:24 -0600

    5

--- app/server.go
+++ app/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -26,40 +28,81 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	for {
+		line := readLine(conn)
+		fmt.Println("L: ", line)
+		if len(line) < 1 {
+			continue
+		}
+		err, resp := handleLine(conn, line)
+		if err != nil {
+			fmt.Println("Error for line: ", line, ", error: ", err.Error())
+		} else {
+			fmt.Println("Resp for line: ", line, ", resp: ", resp)
+			conn.Write([]byte(resp))
+		}
+	}
+}
+
+func handleLine(conn net.Conn, line string) (error, string) {
+	if strings.HasPrefix(line, "*") {
+		sizeStr := line[1:]
+		size, err := strconv.Atoi(sizeStr)
+		if err != nil {
+			return err, ""
+		}
+		return handleArray(conn, size)
+	}
+	return fmt.Errorf("Unknown line command '%s'", line), ""
+}
+
+func handleArray(conn net.Conn, size int) (error, string) {
+	commandSizeLine := readLine(conn)
+	fmt.Println("command size line: ", commandSizeLine)
+	commandLine := strings.ToLower(readLine(conn))
+	if commandLine == "ping" {
+		return nil, "+PONG\r\n"
+	}
+	if commandLine == "echo" {
+		echoStrSizeLine := readLine(conn)
+		fmt.Println("echo string size line: ", echoStrSizeLine)
+		echoArg := strings.ToLower(readLine(conn))
+		return nil, "+" + echoArg + "\r\n"
+	}
+	return fmt.Errorf("Unknown line command '%s'", commandLine), ""
+}
+
+func readLine(conn net.Conn) string {
 	line := ""
 	buff := make([]byte, 1)
 	for {
 		n, err := conn.Read(buff)
 		if err != nil {
-			fmt.Println("Error reading: ", err.Error())
-			break
+			continue
 		}
 		if n < 1 {
 			continue
 		}
 		ch := string(buff)
-		fmt.Println("ch: ", ch)
-		if buff[0] == '\n' || buff[0] == '\r' {
-			if len(line) == 0 {
+		if buff[0] == '\r' {
+			n, err := conn.Read(buff)
+			if err != nil {
+				return ""
+			}
+			if n < 1 {
 				continue
 			}
-			err, resp := handleLine(line)
-			if err != nil {
-				fmt.Println("Error for line: ", line, ", error: ", err.Error())
+			if buff[0] == '\n' {
+				if len(line) == 0 {
+					continue
+				}
+				return line
 			} else {
-				fmt.Println("Resp for line: ", line, ", resp: ", resp)
-				conn.Write([]byte(resp))
+				line += ch
+				line += string(buff[0])
 			}
-			line = ""
 		} else {
 			line += ch
 		}
 	}
-}
-
-func handleLine(line string) (error, string) {
-	if line == "PING" || line == "ping" {
-		return nil, "+PONG\r\n"
-	}
-	return fmt.Errorf("Unknown command '%s'", line), ""
 }
