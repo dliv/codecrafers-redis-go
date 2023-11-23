c31d2b27755eec24423b749de163e92f0f5e9cfd
David Livingston <git@dliv.org>
2023-11-23 14:59:55 -0600

    3

--- app/server.go
+++ app/server.go
@@ -12,12 +12,54 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+		} else {
+			go handleConnection(conn)
+		}
 	}
+}
 
-	conn.Write([]byte("+PONG\r\n"))
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	line := ""
+	buff := make([]byte, 1)
+	for {
+		n, err := conn.Read(buff)
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			break
+		}
+		if n < 1 {
+			continue
+		}
+		ch := string(buff)
+		fmt.Println("ch: ", ch)
+		if buff[0] == '\n' || buff[0] == '\r' {
+			if len(line) == 0 {
+				continue
+			}
+			err, resp := handleLine(line)
+			if err != nil {
+				fmt.Println("Error for line: ", line, ", error: ", err.Error())
+			} else {
+				fmt.Println("Resp for line: ", line, ", resp: ", resp)
+				conn.Write([]byte(resp))
+			}
+			line = ""
+		} else {
+			line += ch
+		}
+	}
+}
+
+func handleLine(line string) (error, string) {
+	if line == "PING" || line == "ping" {
+		return nil, "+PONG\r\n"
+	}
+	return fmt.Errorf("Unknown command '%s'", line), ""
 }
